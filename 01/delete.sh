f62960782775bf62ceb56d7bc420fc5aa7be9f54
nitinww <81169104+nitinww@users.noreply.github.com>
2023-10-03 05:30:04 -0400

    Add files via upload

--- 01/delete.sh
+++ 01/delete.sh
@@ -1,10 +1,14 @@
-echo enter file name
-read file
-echo enter word
-read word
-echo file before removing $word:
-cat $file
-grep -v -i $word $file > test
-mv test $file
-echo file after removing $word:
-cat $file
+echo enter number of files
+read n
+for ((i = 1; i <= $n; i++)); do
+	echo enter file name
+	read file
+	echo enter word
+	read word
+	echo file before removing $word:
+	cat $file
+	grep -v -i $word $file > test
+	mv test $file
+	echo file after removing $word:
+	cat $file
+done
