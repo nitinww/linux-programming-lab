5c9ce445b4d9c139ef2e5cb615d9bd6492c80550
nitinww <81169104+nitinww@users.noreply.github.com>
2023-10-03 05:58:22 -0400

    Delete 01 directory

--- 01/display.sh
+++
@@ -1,9 +0,0 @@
-echo "enter file name: "
-read fname
-echo "enter the starting line number: "
-read s
-echo "enter the ending line number: "
-read e
-# sed -n $s,$e\p $fname
-awk "NR >= $s && NR <= $e" "$fname"
-
