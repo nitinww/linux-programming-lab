c18a24a776342b818c00c97b3984d3322202b19c
nitinww <81169104+nitinww@users.noreply.github.com>
2023-10-03 05:42:05 -0400

    Add files via upload

--- 01/delete.sh
+++ 01/delete.sh
@@ -5,10 +5,7 @@ read word
 for ((i = 1; i <= $n; i++)); do
 	echo enter file name
 	read file
-	echo file before removing $word:
 	cat $file
-	grep -v -i $word $file > test
-	mv test $file
-	echo file after removing $word:
+	sed -i "/$word/d" "$file"
 	cat $file
 done
