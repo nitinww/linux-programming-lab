1167b9d5051925c4a321481b54c47281f9321929
nitinww <81169104+nitinww@users.noreply.github.com>
2023-11-14 16:08:03 +0530

    Add files via upload

---
+++ week - 6/week-6-1.c
@@ -0,0 +1,4 @@
+week-6-1.c
+week-6-2.c
+week-6-3
+week-6-3.c
---
+++ week - 6/week-6-2.c
@@ -0,0 +1,10 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+int main(int argc, char *argv[]) {
+	char d[50];
+	strcat(d, "ls -i ");
+	strcat(d, argv[1]);
+	system(d);
+}
